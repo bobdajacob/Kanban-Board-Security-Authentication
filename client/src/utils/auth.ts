05f238868520b6605f281e9a98eae6ed8c2f1b09
Bob <bobjacob23@yahoo.com>
2024-11-18 11:54:04 -0800

    fixed auth.ts on server

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,42 +1,54 @@
-import { JwtPayload, jwtDecode } from 'jwt-decode';
+// use this to decode a token and get the user's information out of it
+import { jwtDecode } from 'jwt-decode';
 
+interface UserToken {
+  name: string;
+  exp: number;
+}
+
+// create a new class to instantiate for a user
 class AuthService {
+  // get user data
   getProfile() {
-    // TODO: return the decoded token
-    const token = this.getToken();
-    return token ? jwtDecode(token) : null;
+    return jwtDecode(this.getToken() || '');
   }
 
+  // check if user's logged in
   loggedIn() {
-    // TODO: return a value that indicates if the user is logged in
+    // Checks if there is a saved token and it's still valid
     const token = this.getToken();
-    return token;
-  }
-  
-  isTokenExpired(token: string) {
-    // TODO: return a value that indicates if the token is expired
-    const decoded: JwtPayload = jwtDecode(token);
-    const currentTime = Date.now() / 1000;
-    return typeof decoded.exp === 'number' && decoded.exp < currentTime;
+    return !!token && !this.isTokenExpired(token); // handwaiving here
   }
 
-  getToken(): string {
-    // TODO: return the token
-    const loggedUser = localStorage.getItem('id_token') || '';
-    return loggedUser;
+  // check if token is expired
+  isTokenExpired(token: string) {
+    try {
+      const decoded = jwtDecode<UserToken>(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } 
+      
+      return false;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  getToken() {
+    // Retrieves the user token from localStorage
+    return localStorage.getItem('id_token');
   }
 
   login(idToken: string) {
-    // TODO: set the token to localStorage
+    // Saves user token to localStorage
     localStorage.setItem('id_token', idToken);
-    // TODO: redirect to the home page
     window.location.assign('/');
   }
 
   logout() {
-    // TODO: remove the token from localStorage
+    // Clear user token and profile data from localStorage
     localStorage.removeItem('id_token');
-    // TODO: redirect to the login page
+    // this will reload the page and reset the state of the application
     window.location.assign('/');
   }
 }
